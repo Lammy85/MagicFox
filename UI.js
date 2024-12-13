b6642853874fe83fd1a2c866a2cf58a3cb986cd1
Lammy85 <daniela.sachs@yahoo.de>
2024-12-13 12:43:10 +0100

    score

--- UI.js
+++ UI.js
@@ -31,7 +31,7 @@ export class UI {
         if (this.game.gameOver) {
             context.textAlign = 'center';
             context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
-            if (this.game.score > this.game.winningScore && this.game.lives > 0) {
+            if (this.game.score >= this.game.winningScore && this.game.lives > 0) {
                 context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5 - 20);
                 context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
                 context.fillText('What are creatures of the night afraid of? YOU!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
